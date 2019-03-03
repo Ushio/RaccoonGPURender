2925177b4016eb3df9430f53e2a1d90aa7090805
ushiostarfish <ushiostarfish@gmail.com>
2019-03-03 17:23:57 +0900

    prepare bvh

---
+++ BVH/src/ofApp.cpp
@@ -0,0 +1,284 @@
+﻿#include "ofApp.h"
+
+#include "assertion.hpp"
+
+class GPUBVH {
+public:
+	GPUBVH(std::shared_ptr<houdini_alembic::AlembicScene> scene):_scene(scene){
+		for (auto o : scene->objects) {
+			if (o->visible == false) {
+				continue;
+			}
+
+			if (_camera == nullptr) {
+				if (auto camera = o.as_camera()) {
+					_camera = camera;
+				}
+			}
+
+			if (auto polymesh = o.as_polygonMesh()) {
+				addPolymesh(polymesh);
+			}
+		}
+	}
+
+	void addPolymesh(houdini_alembic::PolygonMeshObject *p) {
+		bool isTriangleMesh = std::all_of(p->faceCounts.begin(), p->faceCounts.end(), [](int32_t f) { return f == 3; });
+		if (isTriangleMesh == false) {
+			printf("skipped non-triangle mesh: %s\n", p->name.c_str());
+			return;
+		}
+
+		glm::dmat4 xform;
+		for (int i = 0; i < 16; ++i) {
+			glm::value_ptr(xform)[i] = p->combinedXforms.value_ptr()[i];
+		}
+		glm::dmat3 xformInverseTransposed = glm::inverseTranspose(xform);
+
+		// add index
+		uint32_t base_index = _points.size();
+		for (auto index : p->indices) {
+			_indices.emplace_back(base_index + index);
+		}
+		// add vertex
+		_points.reserve(_points.size() + p->P.size());
+		for (auto srcP : p->P) {
+			glm::vec3 p = xform * glm::dvec4(srcP.x, srcP.y, srcP.z, 1.0);
+			_points.emplace_back(p);
+		}
+
+		RT_ASSERT(std::all_of(_indices.begin(), _indices.end(), [&](uint32_t index) { return index < _points.size(); }));
+
+		// add material
+		//auto Le = p->primitives.column_as_vector3("Le");
+		//auto Cd = p->primitives.column_as_vector3("Cd");
+		//RT_ASSERT(Le);
+		//RT_ASSERT(Cd);
+		//for (uint32_t i = 0, n = p->primitives.rowCount(); i < n; ++i) {
+		//	Material m;
+		//	Le->get(i, glm::value_ptr(m.Le));
+		//	Cd->get(i, glm::value_ptr(m.Le));
+
+		//	_materials.emplace_back(m);
+		//}
+	}
+
+	std::shared_ptr<houdini_alembic::AlembicScene> _scene;
+	houdini_alembic::CameraObject *_camera = nullptr;
+
+	std::vector<uint32_t> _indices;
+	std::vector<glm::vec3> _points;
+};
+
+GPUBVH *_renderer = nullptr;
+
+//--------------------------------------------------------------
+void ofApp::setup() {
+	ofxRaccoonImGui::initialize();
+
+	_camera.setNearClip(0.1f);
+	_camera.setFarClip(100.0f);
+	_camera.setDistance(5.0f);
+
+	houdini_alembic::AlembicStorage storage;
+	std::string error_message;
+	storage.open(ofToDataPath("rungholt.abc"), error_message);
+
+	if (storage.isOpened()) {
+		std::string error_message;
+		_alembicscene = storage.read(0, error_message);
+	}
+	if (error_message.empty() == false) {
+		printf("sample error_message: %s\n", error_message.c_str());
+	}
+
+	_camera_model.load("../../../scenes/camera_model.ply");
+
+	_renderer = new GPUBVH(_alembicscene);
+}
+void ofApp::exit() {
+	ofxRaccoonImGui::shutdown();
+}
+
+//--------------------------------------------------------------
+void ofApp::update() {
+
+}
+
+inline bool isPowerOfTwo(uint32_t n) {
+	return (n & (n - 1)) == 0;
+}
+
+//--------------------------------------------------------------
+void ofApp::draw() {
+	static bool show_scene_preview = true;
+	static int frame = 0;
+
+	//if (_renderer) {
+	//	_renderer->step();
+
+	//	ofDisableArbTex();
+
+	//	//if (ofGetFrameNum() % 5 == 0) {
+	//	//	_image.setFromPixels(toOf(_renderer->_image));
+	//	//}
+	//	//uint32_t n = _renderer->stepCount();
+	//	//if (32 <= n && isPowerOfTwo(n)) {
+	//	//	_image.setFromPixels(toOf(_renderer->_image));
+	//	//	char name[64];
+	//	//	sprintf(name, "%dspp.png", n);
+	//	//	_image.save(name);
+	//	//	printf("elapsed %fs\n", ofGetElapsedTimef());
+	//	//}
+
+	//	ofEnableArbTex();
+	//}
+
+	ofEnableDepthTest();
+
+	ofClear(0);
+
+	_camera.begin();
+	ofPushMatrix();
+	ofRotateZDeg(90.0f);
+	ofSetColor(64);
+	ofDrawGridPlane(1.0f);
+	ofPopMatrix();
+
+	ofDrawAxis(50);
+
+	ofSetColor(255);
+
+	if (_alembicscene && show_scene_preview) {
+		drawAlembicScene(_alembicscene.get(), _camera_model, true /*draw camera*/);
+	}
+
+	//{
+	//	static ofMesh mesh;
+	//	mesh.clear();
+	//	mesh.setMode(OF_PRIMITIVE_TRIANGLES);
+	//	
+	//	for (auto p : _renderer->_points) {
+	//		mesh.addVertex(glm::vec3(p.x, p.y, p.z));
+	//	}
+
+	//	for (auto index : _renderer->_indices) {
+	//		mesh.addIndex(index);
+	//	}
+
+	//	// Houdini は CW
+	//	glFrontFace(GL_CW);
+	//	glEnable(GL_CULL_FACE);
+	//	{
+	//		// 表面を明るく
+	//		ofSetColor(128);
+	//		glCullFace(GL_BACK);
+	//		mesh.draw();
+
+	//		// 裏面を暗く
+	//		ofSetColor(32);
+	//		glCullFace(GL_FRONT);
+	//		mesh.draw();
+	//	}
+	//	glDisable(GL_CULL_FACE);
+
+	//	glEnable(GL_POLYGON_OFFSET_LINE);
+	//	glPolygonOffset(-0.1f, 1.0f);
+	//	ofSetColor(64);
+	//	mesh.clearColors();
+	//	mesh.drawWireframe();
+	//	glDisable(GL_POLYGON_OFFSET_LINE);
+
+	//	ofPopMatrix();
+	//}
+
+	_camera.end();
+
+	ofDisableDepthTest();
+	ofSetColor(255);
+
+	ofxRaccoonImGui::ScopedImGui imgui;
+
+	// camera control                                          for control clicked problem
+	if (ImGui::IsWindowHovered(ImGuiHoveredFlags_AnyWindow) || (ImGui::IsWindowFocused(ImGuiFocusedFlags_AnyWindow) && ImGui::IsAnyMouseDown())) {
+		_camera.disableMouseInput();
+	}
+	else {
+		_camera.enableMouseInput();
+	}
+
+	ImGui::SetNextWindowPos(ImVec2(20, 20), ImGuiCond_Appearing);
+	ImGui::SetNextWindowSize(ImVec2(600, 600), ImGuiCond_Appearing);
+	ImGui::SetNextWindowCollapsed(false, ImGuiCond_Appearing);
+	ImGui::SetNextWindowBgAlpha(0.5f);
+
+	ImGui::Begin("settings", nullptr);
+	ImGui::Checkbox("scene preview", &show_scene_preview);
+	
+	ImGui::Text("frame : %d", frame);
+	//ImGui::Separator();
+	//ImGui::Text("%d sample, fps = %.3f", _renderer->stepCount(), ofGetFrameRate());
+	//ImGui::Text("%d bad sample nan", _renderer->badSampleNanCount());
+	//ImGui::Text("%d bad sample inf", _renderer->badSampleInfCount());
+	//ImGui::Text("%d bad sample neg", _renderer->badSampleNegativeCount());
+	//ImGui::Text("%d bad sample firefly", _renderer->badSampleFireflyCount());
+	//if (_image.isAllocated()) {
+	//	ofxRaccoonImGui::image(_image);
+	//}
+	ImGui::End();
+}
+
+//--------------------------------------------------------------
+void ofApp::keyPressed(int key) {
+
+}
+
+//--------------------------------------------------------------
+void ofApp::keyReleased(int key) {
+
+}
+
+//--------------------------------------------------------------
+void ofApp::mouseMoved(int x, int y) {
+
+}
+
+//--------------------------------------------------------------
+void ofApp::mouseDragged(int x, int y, int button) {
+
+}
+
+//--------------------------------------------------------------
+void ofApp::mousePressed(int x, int y, int button) {
+
+}
+
+//--------------------------------------------------------------
+void ofApp::mouseReleased(int x, int y, int button) {
+
+}
+
+//--------------------------------------------------------------
+void ofApp::mouseEntered(int x, int y) {
+
+}
+
+//--------------------------------------------------------------
+void ofApp::mouseExited(int x, int y) {
+
+}
+
+//--------------------------------------------------------------
+void ofApp::windowResized(int w, int h) {
+
+}
+
+//--------------------------------------------------------------
+void ofApp::gotMessage(ofMessage msg) {
+
+}
+
+//--------------------------------------------------------------
+void ofApp::dragEvent(ofDragInfo dragInfo) {
+
+}
---
+++ BVH/src/ofApp.h
@@ -0,0 +1,34 @@
+﻿#pragma once
+
+#include "ofMain.h"
+
+#include "ofxRaccoonImGui.hpp"
+#include "houdini_alembic.hpp"
+#include "alembic_preview.hpp"
+
+class ofApp : public ofBaseApp{
+public:
+	void setup();
+	void exit();
+
+	void update();
+	void draw();
+
+	void keyPressed(int key);
+	void keyReleased(int key);
+	void mouseMoved(int x, int y );
+	void mouseDragged(int x, int y, int button);
+	void mousePressed(int x, int y, int button);
+	void mouseReleased(int x, int y, int button);
+	void mouseEntered(int x, int y);
+	void mouseExited(int x, int y);
+	void windowResized(int w, int h);
+	void dragEvent(ofDragInfo dragInfo);
+	void gotMessage(ofMessage msg);
+
+	ofEasyCam _camera;
+	ofImage _image;
+	ofMesh _camera_model;
+
+	std::shared_ptr<houdini_alembic::AlembicScene> _alembicscene;
+};

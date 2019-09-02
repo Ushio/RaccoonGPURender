84823f177cb05ac40035cb6353d6cb004abddfec
ushiostarfish <ushiostarfish@gmail.com>
2019-09-02 22:22:13 +0900

    add deploy

---
+++ deploy.bat
@@ -0,0 +1,8 @@
+robocopy Render/bin/ ../rtcamp7_ushio/Render/bin/
+robocopy kernels/ ../rtcamp7_ushio/kernels/
+robocopy scenes/ ../rtcamp7_ushio/scenes/ rtcamp.abc
+robocopy scenes/textures/ ../rtcamp7_ushio/scenes/textures/
+del ..\rtcamp7_ushio\output_images\*.png
+del ..\rtcamp7_ushio\output_images\*.json
+
+
